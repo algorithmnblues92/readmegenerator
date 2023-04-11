f205367ef090de73e800772578d12a0ee961a5e7
Ben <algorithmnblues92@gmail.com>
2023-04-10 23:10:20 -0700

    version 1.0

---
+++ README.md
@@ -0,0 +1,72 @@
+
+# Read Me Generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+
+## Table of Contents
+
+1.[Description](#description)
+2.[Installation Instructions](#iI)
+3.[Usage Information](#uI)
+4.[Contribution Guidelines](#cG)
+5.[Test Instructions](#tI)
+6.[License](#l)
+7.[Screenshots](#s)
+8.[Questions](#q)
+    1.[GitHub User Name](#gUN)
+    2.[Email](#e)
+    3.[Deployed Link](#dL)
+
+<a id="description"/>
+## Description 
+
+This is a template for creating a read me generator for your git hub project, it has inputs and multiple choice and the ability to add screenshots as well as license badges. User will be asked for the title of their project, description of project, installation information, usage information, contribution guidelines, license, up to three screenshots, github user name, email and deployed website link.
+
+<a id="iI"/>
+## Installation Instructions
+
+User will be required to have node.js and package inquirer@8.2.4 installed into your project.
+
+<a id="uI"/>
+## Usage Information
+
+For those who would like an easy way to generate a readme for their project, create then move to folder of your choice.
+
+<a id="cG"/>
+## Contribution Guidelines
+
+This is a solo mission.
+
+<a id="tI"/>
+## Test Instructions
+
+If you find any bugs go ahead and meet me at the corner of here and there and send smoke signals after rubbing two Hurricane tall cans together and I will appear from a portal.
+
+<a id="l"/>
+## License
+
+MIT License
+(https://opensource.org/licenses/MIT)
+
+<a id="s"/>
+## Screenshots
+
+[![Screenshot 1](images/readme_terminal.png)]
+[![Screenshot 2](images/readme_file1.png)]
+[![Screenshot 3](images/readme_file2.png)]
+
+<a id="q"/>
+## Questions
+<a id="gUN"/>
+###Github User Name
+
+algorithmnblues92
+<a id="e"/>
+###Email
+
+notavailable@notavailable.com
+<a id="dL"/>
+###Deployed Link:
+
+https://algorithmnblues92.github.io/readmegenerator/
+
+ 
---
+++ index.js
@@ -0,0 +1,247 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+// TODO: Create an array of questions for user input
+// const questions = [];
+
+
+inquirer.prompt([
+ {
+  name: 'title',
+  message: 'What is the title of your project?',
+  type: 'input',
+ },
+ {
+  name: 'description',
+  message: 'Add a project description:',
+  type: 'input',
+ },
+ {
+  name: 'installationInstructions',
+  message: 'Add installation Information:',
+  type: 'input',
+ },
+ {
+  name: 'usageInformation',
+  message: 'Add Usage Information:',
+  type: 'input',
+ },
+ {
+  name: 'contributionGuidelines',
+  message: 'Add Contribution Guidelines:',
+  type: 'input',
+ },
+ {
+  name: 'testInstructions',
+  message: 'Add Test Instructions:',
+  type: 'input',
+ },
+  {
+  name: 'license',
+  message: 'Add a license:',
+  choices: [
+   'Apache',
+   'GNU General Public License 3.0',
+   'MIT License',
+   'BSD 2-Clause "Simplified" License',
+   'BSD 3-Clause "New" or "Revised" License',
+   'Boost Software License 1.0',
+   'Creative Commons Zero v1.0 Universal',
+   'Eclipse Public License 2.0',
+   'GNU Affero General Public License v3.0',
+   'GNU General Public License v2.0',
+   'GNU Lesser General Public License v2.1',
+   'Mozilla Public License 2.0',
+   'The Unlicense',
+  ],
+  type: 'checkbox',
+ },
+   {
+  name: 'screenshots',
+  message: 'Add screenshots 1 or leave empty:',
+  type: 'input',
+ },
+    {
+  name: 'screenshots2',
+  message: 'Add screenshots 2 or leave empty:',
+  type: 'input',
+ },
+    {
+  name: 'screenshots3',
+  message: 'Add screenshots 3 or leave empty:',
+  type: 'input',
+ },
+  {
+  name: 'githubUserName',
+  message: 'Add github user name:',
+  type: 'input',
+ },
+  {
+  name: 'email',
+  message: 'Add email:',
+  type: 'input',
+ },
+   {
+  name: 'deployedLink',
+  message: 'Add deployed website link:',
+  type: 'input',
+ },
+]).then(res => {
+//  console.log(`${res.license}`);
+//  console.log(`${res.title}`);
+
+//TODO: write for loop, make a few screenshots and add deployed link, generate my own readme then turn it in
+for (i=0;i<licenseObj.length; i++) {
+ if (`${res.license}` === Object.keys(licenseObj)[i]) {
+  console.log('object :' + licenseObj[res.license]);
+ }
+}
+
+ fs.writeFile('README.md', readmeGenerator(res), (err) => {
+  err ? console.error(err) : console.log('Success');
+ } )
+})
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+const readmeGenerator = ({title, description, installationInstructions, usageInformation, contributionGuidelines, testInstructions, license, screenshots, screenshots2, screenshots3, githubUserName, email, deployedLink, ...rest}) => {
+ return `
+# ${title}
+${licenseObj[license]}
+
+## Table of Contents
+
+1.[Description](#description)
+2.[Installation Instructions](#iI)
+3.[Usage Information](#uI)
+4.[Contribution Guidelines](#cG)
+5.[Test Instructions](#tI)
+6.[License](#l)
+7.[Screenshots](#s)
+8.[Questions](#q)
+    1.[GitHub User Name](#gUN)
+    2.[Email](#e)
+    3.[Deployed Link](#dL)
+
+<a id="description"/>
+## Description 
+
+${description}
+
+<a id="iI"/>
+## Installation Instructions
+
+${installationInstructions}
+
+<a id="uI"/>
+## Usage Information
+
+${usageInformation}
+
+<a id="cG"/>
+## Contribution Guidelines
+
+${contributionGuidelines}
+
+<a id="tI"/>
+## Test Instructions
+
+${testInstructions}
+
+<a id="l"/>
+## License
+
+${license}
+${licenseLink[license]}
+
+<a id="s"/>
+## Screenshots
+
+${screenshots}
+${screenshots2}
+${screenshots3}
+
+<a id="q"/>
+## Questions
+<a id="gUN"/>
+###Github User Name
+
+${githubUserName}
+<a id="e"/>
+###Email
+
+${email}
+<a id="dL"/>
+###Deployed Link:
+
+${deployedLink}
+
+ `
+}
+
+
+//TODO: Make array for license badge and links
+
+const licenseObj = {
+ Apache: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]",
+
+ 'GNU General Public License 3.0': "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]",
+
+ 'MIT License': "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]",
+
+ 'BSD 2-Clause "Simplified" License': "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]",
+
+ 'BSD 3-Clause "New" or "Revised" License': "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]",
+
+ 'Boost Software License 1.0': "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]",
+
+  'Creative Commons Zero v1.0 Universal': "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)]",
+
+  'Eclipse Public License 2.0': "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]",
+
+  'GNU Affero General Public License v3.0': "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]",
+
+  'GNU General Public License v2.0': "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)]",
+
+  'GNU Lesser General Public License v2.1': "[![License: GPL v2.1](https://img.shields.io/badge/License-GPL_v2.1-blue.svg)]",
+
+  'Mozilla Public License 2.0': "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]",
+
+  'The Unlicense': "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]",
+
+}
+
+const licenseLink = {
+ Apache: "(https://opensource.org/licenses/Apache-2.0)",
+
+ 'GNU General Public License 3.0': "(https://www.gnu.org/licenses/gpl-3.0)",
+
+ 'MIT License': "(https://opensource.org/licenses/MIT)",
+
+ 'BSD 2-Clause "Simplified" License': "(https://opensource.org/licenses/BSD-2-Clause)",
+ 
+ 'BSD 3-Clause "New" or "Revised" License': "(https://opensource.org/licenses/BSD-3-Clause)",
+
+ 'Boost Software License 1.0': "(https://www.boost.org/LICENSE_1_0.txt)",
+
+ 'Creative Commons Zero v1.0 Universal': "(http://creativecommons.org/publicdomain/zero/1.0/)",
+
+  'Eclipse Public License 2.0': "(https://opensource.org/licenses/EPL-1.0)",
+
+  'GNU Affero General Public License v3.0': "(https://www.gnu.org/licenses/gpl-3.0)",
+
+  'GNU General Public License v2.0': "(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)",
+
+  'GNU Lesser General Public License v2.1': "(https://opensource.org/license/lgpl-2-1/)",
+
+  'Mozilla Public License 2.0': "(https://opensource.org/licenses/MPL-2.0)",
+
+  'The Unlicense': "(http://unlicense.org/)",
+
+}
+
+
+
